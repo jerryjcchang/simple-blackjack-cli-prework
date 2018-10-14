58be63bd2dd1bfa540a5167dff35a7d0709a26b8
JC Chang <jerryjcchang@gmail.com>
2018-10-14 21:00:14 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -71,7 +71,7 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-# def runner
-#   # code runner here
-# end
+def runner
+  loop do
+    
     
